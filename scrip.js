d7505d2277ed6dfffade13cf272d18df55578113
Mikky91 <melisa.ahumada91@gmail.com>
2023-04-16 01:26:39 -0300

    add: funciones

--- scrip.js
+++ scrip.js
@@ -4,7 +4,18 @@ const myInput = document.getElementById('myInput')
 myModal.addEventListener('shown.bs.modal', () => {
   myInput.focus()
 })
+
+//Registro usuario 
+function getFormData(){
+  var config = {};
+   $('input').each(function () {
+    config[this.name] = this.value;
+   });
+   console.log(config);
+}
+
 // formulario
+  //validacion 
 $(document).ready(function() {
   $("#basic-form").validate();
 });
